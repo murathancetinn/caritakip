7527bfa2c4441fc0f2a6812ce4d38b5529925932
murathancetinn <129846458+murathancetinn@users.noreply.github.com>
2026-04-05 15:07:08 +0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_CONFIG = {
   url: "https://lkbdhwqnlgfzrchomzzs.supabase.co",
-  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxrYmRod3FubGdmenJjaG9tenpzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUzODk4MjIsImV4cCI6MjA5MDk2NTgyMn0.rJwJBU37mzEazWFJvgsiNws9x4cpUEK3N4vCVQKU45U"
+  anonKey: "sb_publishable_uDWBTDyLnoecq_n2a1uPMg_voX8tI3k"
 };
