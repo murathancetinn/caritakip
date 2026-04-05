e980167c1845eeec82a17211d0120e8de8c8b52c
murathancetinn <129846458+murathancetinn@users.noreply.github.com>
2026-04-05 15:05:41 +0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_CONFIG = {
-  url: "YOUR_SUPABASE_URL",
-  anonKey: "YOUR_SUPABASE_ANON_KEY"
+  url: "https://lkbdhwqnlgfzrchomzzs.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxrYmRod3FubGdmenJjaG9tenpzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUzODk4MjIsImV4cCI6MjA5MDk2NTgyMn0.rJwJBU37mzEazWFJvgsiNws9x4cpUEK3N4vCVQKU45U"
 };
